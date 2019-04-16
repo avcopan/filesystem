d4285be3c6a1545244ade0676edbdfbc3993f878
avcopan <avcopan@gmail.com>
2019-04-16 16:08:38 -0500

    Increments version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 
 setup(name="filesystem",
-      version="0.1.5",
+      version="0.1.6",
       packages=["autodir",
                 "autoinf",
                 "autofile"])
